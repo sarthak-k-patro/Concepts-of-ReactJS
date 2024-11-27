8179d1d3cd87bb73989bd6ba3782c1995226babd
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2024-11-28 00:28:27 +0530

    Using useEffect and code cleanup for optimising code.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import styles from "./";
 import ImageSlider from "./ImageSlider";
-
+import React from "react";
 function App() {
   return (
-    <>
+    <React.StrictMode>
       <div>
         <ImageSlider />
       </div>
-    </>
+    </React.StrictMode>
   );
 }
 
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,37 +1,51 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
 const ImageSlider = () => {
   const [users, setUsers] = useState(null);
   const [color, setColor] = useState("red");
+
+  const insideSetUsers = (data) => {
+    console.log("setting Users using setUser");
+    setUsers(data);
+  };
   useEffect(() => {
+    let isMounted = true; // Track if the component is mounted
     fetch("https://jsonplaceholder.typicode.com/albums")
       .then((response) => response.json())
       .then((data) => {
-        setUsers(data);
-        console.log("respinse   kjsnkjsnkj:  ", data);
+        if (isMounted) {
+          // Only set state if mounted
+          insideSetUsers(data);
+          console.log("response: ", data);
+        }
       })
       .catch((err) => {
         console.log(err);
       });
+
+    return () => {
+      isMounted = false; // Cleanup function sets isMounted to false
+    };
   }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColor((prevColor) => (prevColor === "red" ? "orange" : "red"));
+    }, 2000);
+
+    return () => clearInterval(intervalId); // Cleanup the interval on unmount
+  }, []); // Empty dependency array to run only once on mount
 
   const h1Style = {
     backgroundColor: color,
   };
-  const changeColor = () => {
-    setColor((prevColor) => (prevColor === "red" ? "orange" : "red"));
-  };
-  // console.log("data outside fetch :", dataToShow)
+
   return (
     <>
       {users &&
-        users.map((users) => (
-          <h1
-            style={h1Style}
-            key={users.id}
-          >{`${users.title} + ${users.id}`}</h1>
+        users.map((user) => (
+          <h1 style={h1Style} key={user.id}>{`${user.title} + ${user.id}`}</h1>
         ))}
-      <button onClick={changeColor}> toggle color </button>
     </>
   );
 };
