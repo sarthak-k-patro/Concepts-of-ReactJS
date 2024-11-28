79f54768882b22594b63fa3d33d42fd776f7bd47
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2024-11-29 01:23:06 +0530

    useEffect hook example with no parameter for dependency

---
+++ src/AllAboutuseEffect.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export const AllAboutuseEffect = () => {
+  const [count, setCount] = useState(0);
+  const [dependencyCount, setDependencyCount] = useState(0);
+  const func = () => {
+    setCount((prev) => prev + 1);
+  };
+  useEffect(func);
+
+  const handleClick = () => {
+    setDependencyCount((prev) => prev + 1);
+    console.log("count value with dependency array: ", dependencyCount);
+  };
+
+  return (
+    <>
+      <h1>
+        {" "}
+        it will run infinitely because no dependency paramter given in useEffect
+        hook : count = {count}
+      </h1>
+      <h2>
+        it will render once and again if you click this button :{" "}
+        {dependencyCount}
+        <br></br>
+        <button onClick={handleClick}> Increase counter </button>
+      </h2>
+      <br></br>
+      <hr></hr>
+    </>
+  );
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import styles from "./";
 import AllAboutuseState from "./AllAboutuseState";
+import { AllAboutuseEffect } from "./AllAboutuseEffect";
 import React from "react";
 function App() {
   return (
     <React.StrictMode>
+      <div>
+        <AllAboutuseEffect />
+      </div>
       <div>
         <AllAboutuseState />
       </div>
